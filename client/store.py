63c6b8f423c4c2afd7d2cb2edaaa33c92aed0765
Krishnanunni333 <kittuvirus65@gmail.com>
2022-11-11 18:34:48 +0000

    created an implementation for upload and listing txt files

--- client/store.py
+++ client/store.py
@@ -1,9 +1,35 @@
 import click
+import upload
 
-@click.command()
-def hello():
-    click.echo('Hello World!')
+@click.group()
+def cli():
+    pass
+
+@cli.command("add")
+@click.argument('files', type=str, nargs=-1)
+def add(files):
+    upload.push(files)
+
+@cli.command("ls")
+def ls():
+    click.echo("ls")
+
+@cli.command("rm")
+def rm():
+    click.echo("rm")
+
+@cli.command("update")
+def update():
+    click.echo("update")
+
+@cli.command("wc")
+def wc():
+    click.echo("wc")
+
+@cli.command("freq-words")
+def freq_words():
+    click.echo("freq-words")
 
 
 if __name__ == '__main__':
-    hello()
+    cli()
--- server/app.py
+++ server/app.py
@@ -1,6 +1,41 @@
-from flask import Flask
+from flask import Flask, request, jsonify, current_app
+import util
+
 
 app = Flask(__name__)
+
+app.md5s = util.get_md5_files()
+
 @app.route('/')
 def hello_world():
     return 'Hello world!'
+
+@app.route('/copyfile', methods=['PUT'])
+def copyfile():
+    existingfile = request.args.get("existingfile")
+    newfile = request.args.get("newfile")
+    util.copyfile(existingfile, newfile)
+    return "Success", 201
+
+
+@app.route('/getmd5sum', methods=['GET'])
+def getmd5sum():
+    return jsonify(current_app.md5s), 200
+
+
+@app.route('/newfilecreation', methods=['POST'])
+def newfilecreation():
+    line_payload = request.get_json()
+    success = util.process_json_payload(line_payload)
+    if success:
+        return "Success", 201
+    return "Failed", 500
+
+
+@app.route('/getalltxtfiles', methods=['GET'])
+def getalltxtfiles():
+    alltxtfiles = util.getalltxtfiles()
+    response_payload = {
+        "txtfiles" : alltxtfiles
+    }
+    return jsonify(response_payload), 200
