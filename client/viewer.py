ebb2530dbcb59d403aa08a08c52f4bc9aebcea46
Krishnanunni333 <kittuvirus65@gmail.com>
2022-11-13 17:07:04 +0000

    changes related to test and documentation

--- client/viewer.py
+++ client/viewer.py
@@ -2,13 +2,13 @@ import click
 
 
 def display_error(content):
-    click.secho("ERROR: {}".format(content), fg="red", bold=True)
+    click.secho("ERROR: \n{}".format(content), fg="red", bold=True)
 
 def display_success(content):
-    click.secho("SUCCESS: {}".format(content), fg="green", bold=True)
+    click.secho("----SUCCESS---- \n{}".format(content), fg="green", bold=True)
 
 def display_warning(content):
-    click.secho("WARNING: {}".format(content), fg="yellow", bold=True)
+    click.secho("WARNING: \n{}".format(content), fg="yellow", bold=True)
 
 def display_files(content):
     click.secho("----------ALL FILES---------\n{}".format(content), fg="blue", bold=True)
